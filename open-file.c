7c6baee354c13fef01335fe4b2b5a5ba0b0ac49c
shambhuKumar <kshambhu67515@gmail.com>
2022-11-05 18:38:25 +0530

    open file , write and append. correct file close bug in previous version

--- open-file.c
+++ open-file.c
@@ -4,13 +4,32 @@
 int main(void)
 {
 	FILE *fp;
-	fp = fopen("test.txt","w");
+	unsigned char result;
+	fp = fopen("test.txt","a");
 	if(fp == NULL)
 	{
 		printf("file open failed!\n");
 		exit(1);
 	}
 	printf("file open success!\n");
+	result = fputs("my name is shambhu kumar\n",fp);
+	if(result == EOF)
+	{	
+		printf("write failed in test.txt\n");
+	}
+	else
+	{	
+		printf("write success in test.txt\n");
+	}
+	result = fclose(fp);
 
+	if(result == EOF)
+	{	
+		printf("file close failed\n");
+	}
+	else
+	{	
+		printf("file close success\n");
+	}
 	return 0;
 }
